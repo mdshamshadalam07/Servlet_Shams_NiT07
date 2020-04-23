c19681b94d0facff6d9d5844e4bdc3d20f4929b9
Win10 <Win10@DESKTOP-OF4EISA>
2020-04-23 23:11:29 +0530

    Servlet

---
+++ 16-MIMEAPP/src/com/nt/servlet/HtmlServlet.java
@@ -0,0 +1,24 @@
+package com.nt.servlet;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class HtmlServlet extends HttpServlet {
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse res)throws ServletException,IOException {
+		// getPrintWrite 
+		   PrintWriter pw=res.getWriter(); 
+	   // setContent type
+		   res.setContentType("text/Html");
+	   // write output message to reponse obj
+	      pw.println("<table broder='0' align='center'>");
+		  pw.println("<tr><th>Player</th><th>Role</th></tr>");
+		  pw.println("<tr><td>Virat Kohli</th><th>Captan</td></tr>");
+		  pw.println("<tr><td>Rohit Sharma</th><th>Opener Bastman</td></tr>");
+		  pw.println("<tr><td>Yousuf Pathan</th><th>Bastman</td></tr>");
+		  pw.println("<tr><td>Irfan Pathan</th><th>All Rounder</td></tr>");
+		  pw.println("<tr><td>Rishabh Pant</th><th>WicketKeeper</td></tr>");
+		  pw.println("<tr><td>Jasprit Bumrah</th><th>Bolwer</td></tr>");
+		  pw.close();
+	} // service(-,-)
+} //class
